6cd07627969255e074b64eace6dde8bd69b10fe1
DomBro <460716809@qq.com>
2024-04-11 00:30:54 +0800

    <feature>(rag): 新建 RAG 包，联系 es rag

---
+++ rag/es_engine.py
@@ -0,0 +1,56 @@
+from elasticsearch7 import Elasticsearch, helpers
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+import re
+from  es_init import to_keywords
+from pdf_helper import extract_text_from_pdf
+
+
+# 1. 创建Elasticsearch连接
+es = Elasticsearch(
+    hosts=['http://117.50.198.53:9200'],  # 服务地址与端口
+    http_auth=("elastic", "FKaB1Jpz0Rlw0l6G"),  # 用户名，密码
+)
+
+# 2. 定义索引名称
+index_name = "teacher_demo_index123"
+
+# 3. 如果索引已存在，删除它（仅供演示，实际应用时不需要这步）
+if es.indices.exists(index=index_name):
+    es.indices.delete(index=index_name)
+
+# 4. 创建索引
+es.indices.create(index=index_name)
+
+paragraphs = extract_text_from_pdf("rag\llama2.pdf", min_line_length=10)
+# 5. 灌库指令
+actions = [
+    {
+        "_index": index_name,
+        "_source": {
+            "keywords": to_keywords(para),
+            "text": para
+        }
+    }
+    for para in paragraphs
+]
+
+# 6. 文本灌库
+helpers.bulk(es, actions)
+
+def search(query_string, top_n=3):
+    # ES 的查询语言
+    search_query = {
+        "match": {
+            "keywords": to_keywords(query_string)
+        }
+    }
+    res = es.search(index=index_name, query=search_query, size=top_n)
+    return [hit["_source"]["text"] for hit in res["hits"]["hits"]]
+
+
+results = search("how many parameters does llama 2 have?", 2)
+for r in results:
+    print(r+"\n")
---
+++ rag/es_init.py
@@ -0,0 +1,28 @@
+from elasticsearch7 import Elasticsearch, helpers
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+import re
+
+import warnings
+
+# 屏蔽 ES 的一些Warnings
+warnings.simplefilter("ignore")  
+
+nltk.download('punkt')  # 英文切词、词根、切句等方法
+nltk.download('stopwords')  # 英文停用词库
+
+# 转换成关键词
+def to_keywords(input_string):
+    '''（英文）文本只保留关键字'''
+    # 使用正则表达式替换所有非字母数字的字符为空格
+    no_symbols = re.sub(r'[^a-zA-Z0-9\s]', ' ', input_string)
+    word_tokens = word_tokenize(no_symbols)
+    # 加载停用词表
+    stop_words = set(stopwords.words('english'))
+    ps = PorterStemmer()
+    # 去停用词，取词根
+    filtered_sentence = [ps.stem(w)
+                         for w in word_tokens if not w.lower() in stop_words]
+    return ' '.join(filtered_sentence)
